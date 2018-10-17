411cde7c9b12f6fc59c2d5da78b00d3cfbbfcda7
Daniel Gittings <dangittings@gmail.com>
2018-10-17 20:50:39 +0100

    Add some tweaks

--- src/components/GameTitle.js
+++ src/components/GameTitle.js
@@ -6,7 +6,8 @@ const MainTitle = styled.h1`
   text-align: center;
   font-family: sans-serif;
   font-size: ${props => props.primary ? "5rem" : "3rem"};
-  margin: 0;
+  margin: 0 0 15px;
+  color: #444444;
 `;
 
 const GameTitle = ({ title, primary }) => {
